da58883a0a81077d6b1176db074ca109ff4dadd7
rodriguezanthony1104 <rodriguezanthony1104@gmail.com>
2025-03-06 16:59:08 -0500

    Main Menu tweaks

---
+++ Prototype 1/Assets/Scripts/StartTransition.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartTransition : MonoBehaviour
+{
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Gameplay Prototype");
+        Debug.Log("Next Scene");
+    }
+}
